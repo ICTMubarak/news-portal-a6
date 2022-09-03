aa56074b10a998b21c170d83fc05378760c7a5e4
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-09-03 07:46:54 +0600

    category wise news array loaded

--- js/news.js
+++ js/news.js
@@ -1,24 +1,40 @@
 const loadNewsCatagory = () => {
-    // const url = https://openapi.programming-hero.com/api/news/categories;
-    fetch('https://openapi.programming-hero.com/api/news/categories')
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    fetch(url)
     .then(res => res.json())
     .then(data => displayCatagory(data))
     // .catch(error => console.log(error))
 }
 
 const displayCatagory = data => {
+    const catagoryID = data.data.news_category.category_id;
+    
     const catagoryArr = data.data.news_category;
-    console.log(catagoryArr);
+    
     const catagoryContainer = document.getElementById('catagory-container');
     catagoryArr.forEach(catagory => {
+        const catagoryId = catagory.category_id; 
+        console.log(catagoryId);
         const catagoryDiv = document.createElement('div');
         catagoryDiv.innerHTML = `
-          <h6>${catagory.category_name}</h6>
+          <button onclick="loadNews(${catagoryId})"><h6>${catagory.category_name}</h6></button>
         `;
         catagoryContainer.appendChild(catagoryDiv);
     })
 }
 
+const loadNews = (catagoryId) =>{
+        const url = `https://openapi.programming-hero.com/api/news/category/0${catagoryId}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayNews(data))
+        // .catch(error => console.log(error))
+
+}
+
+const displayNews = (data) => {
+    console.log(data);
+}
 
 
 
